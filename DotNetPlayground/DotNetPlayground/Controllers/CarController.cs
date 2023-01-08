3e56f4e9bd00bc9464b6d3bc97f7ee0981e00087
Amor Osmic <amor.osmic@edu.fit.ba>
2023-01-09 00:06:51 +0100

    Napravljen zadatak

---
+++ DotNetPlayground/DotNetPlayground/Controllers/CarController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetPlayground.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CarController : ControllerBase
+{
+    /*
+        Napraviti API endpoint-e (rute) unutar ovog controller-a koji će omogućit da se prikažu
+        sva vozila koja su snimljena u bazu podataka, da se mogu kreirati nova vozila, da se mogu obrisati
+        i da se mogu editovati vozila.
+        
+        Model vozila je isti kao što je na Angular strani. Prikaz informacija o kreiranom vozilu treba da bude isti kao
+        i kod tabelarnog prikaza u Angular-u. 
+        Npr.
+            Tip: Karavan,
+            Marka: MojaMarka
+            DatumKreiranja: 09.01.2023.
+
+        Dozvoljeno je sve resurse koristiti. :D
+        Za sva pitanja, obratite se najbližem lijenom... instruktoru... ili mentoru... ili kako god xD.
+        
+        Have fun coding ^_^
+    */
+}
