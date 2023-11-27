b7212576c7732a280bdff72ecacd6adcb5cdbb83
Anersyum <amorosmic@gmx.com>
2023-11-27 21:20:10 +0100

    Identity server

---
+++ DotNetPlayground/IdentityServer/IdentityProfile.cs
@@ -0,0 +1,23 @@
+﻿using IdentityServer4.Extensions;
+using IdentityServer4.Models;
+using IdentityServer4.Services;
+using System.Security.Claims;
+
+namespace IdentityServer;
+
+public sealed class IdentityProfile : IProfileService
+{
+    public Task GetProfileDataAsync(ProfileDataRequestContext context)
+    {
+        var sub = context.Subject.GetSubjectId();
+
+        context.IssuedClaims = new() { new Claim("mojClaim", "meho") };
+
+        return Task.CompletedTask;
+    }
+
+    public Task IsActiveAsync(IsActiveContext context)
+    {
+        return Task.CompletedTask;
+    }
+}
---
+++ DotNetPlayground/IdentityServer/TokenValidator.cs
@@ -0,0 +1,20 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Validation;
+using System.Security.Claims;
+
+namespace IdentityServer;
+
+public class TokenValidator : ICustomTokenRequestValidator
+{
+    public Task ValidateAsync(CustomTokenRequestValidationContext context)
+    {
+        var testValue = context.Result.ValidatedRequest.Raw.Get("test");
+
+        if (!string.IsNullOrEmpty(testValue))
+        {
+            context.Result.ValidatedRequest.ClientClaims = new List<Claim> { new("test", testValue!) };
+        }
+
+        return Task.CompletedTask;
+    }
+}
