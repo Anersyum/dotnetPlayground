62019e1e42ad8ccc56c0e081f54ff7f7fa28ab68
Anersyum <amorosmic@gmx.com>
2023-12-07 01:06:39 +0100

    Changed the awaitable pattern demo

--- DotNetPlayground/Asynchrony/Program.cs
+++ DotNetPlayground/Asynchrony/Program.cs
@@ -1,10 +1,12 @@
 ﻿using Asynchrony;
 using Asynchrony.Demos;
+using AwaitablePattern;
+
 // using AwaitablePattern;
 
 // TasksExamplesDemo examplesDemo = new();
 // await examplesDemo.RunTask();
-await ContinueWithDemo.Run();
+// await ContinueWithDemo.Run();
 // await CancellationTokenDemo.Run();
 
 // exception without awaiting
@@ -29,16 +31,16 @@ await ContinueWithDemo.Run();
 // stateMachine.MoveNext(Command.Process);
 // stateMachine.MoveNext(Command.Done);
 
-// Entry();
-// static async CustomTask Entry()
-// {
-//     CustomTask customTask = new(() =>
-//     {
-//         Thread.Sleep(2000);
-//         Console.WriteLine("Inside the action {0}", Thread.CurrentThread.ManagedThreadId);
-//     });
-//
-//     await customTask;
-//
-//     Console.WriteLine("After CustomTask awaited");
-// }
+Entry();
+static async CustomTask Entry()
+{
+    CustomTask customTask = new(() =>
+    {
+        Thread.Sleep(2000);
+        Console.WriteLine("Inside the action {0}", Thread.CurrentThread.ManagedThreadId);
+    });
+
+    await customTask;
+
+    Console.WriteLine("After CustomTask awaited");
+}
